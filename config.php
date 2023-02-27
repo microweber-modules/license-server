a0c79ab36da665854b7fb7b9795fe118c633b863
Bozhidar Slaveykov <bobi@microweber.com>
2023-02-27 13:15:36 +0200

    update

---
+++ config.php
@@ -0,0 +1,25 @@
+<?php
+$config = array();
+$config['name'] = "Lincese Server";
+$config['author'] = "Microweber";
+
+$config['categories'] = "admin";
+$config['version'] = 0.1;
+$config['ui_admin'] = true;
+$config['ui'] = false;
+$config['position'] = 99;
+
+$config['settings'] = [];
+$config['settings']['routes'] = [
+    'admin'=>'admin.license_server.index'
+];
+
+$config['settings']['autoload_namespace'] = [
+    [
+        'path' => __DIR__ . '/src/',
+        'namespace' => 'MicroweberPackages\\Modules\\LicenseServer\\'
+    ],
+];
+$config['settings']['service_provider'] = [
+    \MicroweberPackages\Modules\LicenseServer\LicenseServerServiceProvider::class
+];
